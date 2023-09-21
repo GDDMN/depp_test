142816d804da5b67125c1ab32215e9444f0669d4
GDDMN <apawkin@gmail.com>
2023-09-21 16:01:03 +0300

    add fx

--- Assets/Scripts/Weapons/Projectile.cs
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
   [SerializeField] private float _speed;
+  [Header("Visual")]
+  [SerializeField] private ParticleSystem destroyParticle;
+  [SerializeField] private ParticleSystem ricochetParticle;
   private Vector2 _direction;
   private Rigidbody2D _rigidbody;
 
@@ -16,5 +19,18 @@ public class Projectile : MonoBehaviour
   private void OnCollisionEnter2D(Collision2D collision)
   {
     _rigidbody.velocity = Vector2.Reflect(_direction, collision.contacts[0].normal) * _speed;
+    var particle = Instantiate(ricochetParticle, transform.position, Quaternion.identity);
   }
+
+  private void OnTriggerEnter2D(Collider2D collision)
+  {
+
+  }
+
+  //private void Destruction()
+  //{
+  //  var particle = Instantiate(destroyParticle, transform.position, Quaternion.identity);
+  //}
+
+
 }
