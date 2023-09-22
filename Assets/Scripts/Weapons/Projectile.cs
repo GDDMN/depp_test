158a2ff2d6021a625335a25a48df78cad3c3ffb5
GDDMN <apawkin@gmail.com>
2023-09-22 16:35:13 +0300

    fix progectile lifetime

--- Assets/Scripts/Weapons/Projectile.cs
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
+using System.Collections;
 
 public class Projectile : MonoBehaviour
 {
+  [Header("Data")]
   [SerializeField] private float _speed;
+  [SerializeField] private float _lifetimeSpeed;
   [Header("Visual")]
   [SerializeField] private ParticleSystem destroyParticle;
   [SerializeField] private ParticleSystem ricochetParticle;
+  
+  private float _lifetime = 1f;
+  private float _time = 0;
+
   private Vector2 _direction;
   private Rigidbody2D _rigidbody;
 
@@ -14,6 +21,24 @@ public class Projectile : MonoBehaviour
     _direction = direction;
     _rigidbody = GetComponent<Rigidbody2D>();
     _rigidbody.velocity = _direction * _speed;
+
+    StartLifeTimeCoroutine();
+  }
+
+  private void StartLifeTimeCoroutine()
+  {
+    StartCoroutine(LifeTime());
+  }
+
+  private IEnumerator LifeTime()
+  {
+    while(_time < _lifetime)
+    {
+      _time += _lifetimeSpeed * Time.deltaTime;
+      yield return null;
+    }
+
+    DestroyProgectile();
   }
 
   private void OnCollisionEnter2D(Collision2D collision)
@@ -23,5 +48,11 @@ public class Projectile : MonoBehaviour
 
     var particle = Instantiate(ricochetParticle, transform.position, Quaternion.identity);
     Destroy(particle, .3f);
+  }
+
+  private void DestroyProgectile()
+  {
+    var particle = Instantiate(destroyParticle, transform.position, Quaternion.identity);
+    Destroy(this.gameObject);
   }
 }
